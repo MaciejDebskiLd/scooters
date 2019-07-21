c48a3eff1a295b6c9b0e318db399f93a4f5dc49f
Maciek Debski <maciej.debski@g2.pl>
2019-07-21 13:01:55 +0200

    Create scooter class

--- src/main/java/com/scooter/objects/Scooter.java
+++ src/main/java/com/scooter/objects/Scooter.java
@@ -1,4 +1,41 @@
 package com.scooter.objects;
 
+import java.util.Objects;
+
 public class Scooter {
+
+    private int id;
+
+    public Scooter(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Scooter scooter = (Scooter) o;
+        return id == scooter.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Scooter{" +
+                "id=" + id +
+                '}';
+    }
 }
+
