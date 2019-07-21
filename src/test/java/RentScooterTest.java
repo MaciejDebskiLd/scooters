3138ce6da3d4f53ea80dba868456bc0c51cc843a
Maciek Debski <maciej.debski@g2.pl>
2019-07-21 15:41:56 +0200

    Create rent scooter test feature

--- src/test/java/RentScooterTest.java
+++ src/test/java/RentScooterTest.java
@@ -1,4 +1,36 @@
+import com.scooter.objects.Scooter;
+import com.scooter.objects.User;
+import com.scooter.utils.RentScooter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RentScooterTest {
+
+    RentScooter rentScooter = new RentScooter();
+
+    @BeforeEach
+    void prepareData(){
+        LocalDateTime t1 = LocalDateTime.parse("2019-07-21T12:00");
+        LocalDateTime t2 = LocalDateTime.parse("2019-07-21T15:20");
+        LocalDateTime t3 = LocalDateTime.now();
+
+        User user = new User();
+        user.setNick("Bolo");
+
+        Scooter scooter1 = new Scooter(1);
+        rentScooter.setStartDateTime(t1);
+        rentScooter.setStopDateTime(t2);
+    }
+
+    @Test
+    void checkCost(){
+
+               assertEquals(60, rentScooter.getCost());
+    }
 
 
 }
