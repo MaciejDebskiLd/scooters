412471d73baaf2a819d01154b2e68a89bc23152e
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-07-21 13:00:35 +0200

    configure User class

--- src/main/java/com/scooter/objects/User.java
+++ src/main/java/com/scooter/objects/User.java
@@ -1,4 +1,39 @@
 package com.scooter.objects;
 
+import java.util.Objects;
+
 public class User {
+
+    private String nick;
+
+    public User() {
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(nick, user.nick);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nick);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nick='" + nick + '\'' +
+                '}';
+    }
 }
